dc5430f6a3c0734d54291f03d17bc24a08d5f278
henribruvier <henri.bruvier@gmail.com>
2022-10-24 10:30:48 +0200

    feat(api): :sparkles: create user router

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -2,9 +2,11 @@
 import {t} from '../trpc';
 
 import {gameRouter} from './game';
+import {userRouter} from './user';
 
 export const appRouter = t.router({
 	game: gameRouter,
+	user: userRouter,
 });
 
 // export type definition of API
---
+++ packages/api/src/router/user.ts
@@ -0,0 +1,22 @@
+import {t} from '../trpc';
+import {z} from 'zod';
+
+export const userRouter = t.router({
+	all: t.procedure.query(({ctx}) => {
+		return ctx.prisma.user.findMany();
+	}),
+	byId: t.procedure.input(z.string()).query(({ctx, input}) => {
+		return ctx.prisma.user.findFirst({where: {id: input}});
+	}),
+	create: t.procedure
+		.input(
+			z.object({
+				name: z.string(),
+				email: z.string(),
+				password: z.string(),
+			}),
+		)
+		.mutation(({ctx, input}) => {
+			return ctx.prisma.user.create({data: input});
+		}),
+});
