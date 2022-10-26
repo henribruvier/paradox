a949a5827805f9e614e8ff2616338b9b18be00e4
henribruvier <henri.bruvier@gmail.com>
2022-10-26 19:53:41 +0200

    feat(db): :boom: new try

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -10,8 +10,8 @@ const trpcHandler = createNextApiHandler({
 });
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	res.setHeader('Access-Control-Allow-Origin', '');
-	res.setHeader('Access-Control-Request-Method', '');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	res.setHeader('Access-Control-Allow-Headers', '*');
 	if (req.method === 'OPTIONS') {
