c158fb654846b869249825dbd95f7c5c7d07d5de
henribruvier <henri.bruvier@gmail.com>
2022-10-26 19:48:37 +0200

    feat(desktop-app): :boom: new try

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -14,6 +14,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	res.setHeader('Access-Control-Request-Method', '');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	res.setHeader('Access-Control-Allow-Headers', '*');
+	if (req.method === 'OPTIONS') {
+		res.writeHead(200);
+		return res.end();
+	}
 	trpcHandler(req, res);
 };
 
