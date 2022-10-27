ce35a591482dc40a150285bca757e6123a830aca
henribruvier <henri.bruvier@gmail.com>
2022-10-27 18:49:49 +0200

    fix(api): :bug: try to make the api vercel compatible

--- apps/expo/src/utils/trpc.tsx
+++ apps/expo/src/utils/trpc.tsx
@@ -1,10 +1,26 @@
 import {createTRPCReact} from '@trpc/react';
-import type {AppRouter} from '@paradox/api';
 
 /**
  * A set of typesafe hooks for consuming your API.
  */
 export const trpc = createTRPCReact<AppRouter>();
+
+/**
+ * Extend this function when going to production by
+ * setting the baseUrl to your production API URL.
+ */
+import Constants from 'expo-constants';
+const getBaseUrl = () => {
+	/**
+	 * Gets the IP address of your host-machine. If it cannot automatically find it,
+	 * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
+	 * you don't have anything else running on it, or you'd have to change it.
+	 */
+	const localhost = Constants.manifest?.debuggerHost?.split(':')[0];
+	if (!localhost)
+		throw new Error('failed to get localhost, configure it manually');
+	return `http://${localhost}:3000`;
+};
 
 /**
  * A wrapper for your app that provides the TRPC context.
@@ -14,6 +30,7 @@ import React from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {httpBatchLink} from '@trpc/client';
 import {transformer} from '@paradox/api/transformer';
+import {AppRouter} from '@paradox/api';
 
 export const TRPCProvider: React.FC<{children: React.ReactNode}> = ({
 	children,
--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,38 @@
 import {createNextApiHandler} from '@trpc/server/adapters/next';
 import {appRouter, createContext} from '@paradox/api';
+import Cors from 'cors';
+import {NextApiRequest, NextApiResponse} from 'next';
 
-// export API handler
-export default createNextApiHandler({
+const trpcHandler = createNextApiHandler({
 	router: appRouter,
 	createContext: createContext,
 });
+
+const cors = Cors({
+	methods: ['POST', 'GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+	req: NextApiRequest,
+	res: NextApiResponse,
+	fn: Function,
+) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result: any) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	await runMiddleware(req, res, cors);
+	trpcHandler(req, res);
+};
+
+export default handler;
