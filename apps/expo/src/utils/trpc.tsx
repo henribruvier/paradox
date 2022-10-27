7c2ea3a63752d24ec247073f5336287a4eb71e63
henribruvier <henri.bruvier@gmail.com>
2022-10-27 16:16:14 +0200

    feat(mobile): :art: update api url

--- apps/expo/src/utils/trpc.tsx
+++ apps/expo/src/utils/trpc.tsx
@@ -1,26 +1,10 @@
 import {createTRPCReact} from '@trpc/react';
 import type {AppRouter} from '@paradox/api';
+
 /**
  * A set of typesafe hooks for consuming your API.
  */
 export const trpc = createTRPCReact<AppRouter>();
-
-/**
- * Extend this function when going to production by
- * setting the baseUrl to your production API URL.
- */
-import Constants from 'expo-constants';
-const getBaseUrl = () => {
-	/**
-	 * Gets the IP address of your host-machine. If it cannot automatically find it,
-	 * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
-	 * you don't have anything else running on it, or you'd have to change it.
-	 */
-	const localhost = Constants.manifest?.debuggerHost?.split(':')[0];
-	if (!localhost)
-		throw new Error('failed to get localhost, configure it manually');
-	return `http://${localhost}:3000`;
-};
 
 /**
  * A wrapper for your app that provides the TRPC context.
@@ -40,7 +24,7 @@ export const TRPCProvider: React.FC<{children: React.ReactNode}> = ({
 			transformer,
 			links: [
 				httpBatchLink({
-					url: `${getBaseUrl()}/api/trpc`,
+					url: `https://paradox-sand.vercel.app/api/trpc`,
 				}),
 			],
 		}),
