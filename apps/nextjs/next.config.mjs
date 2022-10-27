b1c1a13dae6e1d19488bacbfc3378416cf6dce25
henribruvier <henri.bruvier@gmail.com>
2022-10-27 09:32:56 +0200

    feat(api): :bug: newx try

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import { env } from "./src/env/server.mjs";
-import withTM from "next-transpile-modules";
+import {env} from './src/env/server.mjs';
+import withTM from 'next-transpile-modules';
 
 /**
  * Don't be scared of the generics here.
@@ -11,12 +11,33 @@ import withTM from "next-transpile-modules";
  * @constraint {{import('next').NextConfig}}
  */
 function defineNextConfig(config) {
-  return config;
+	return config;
 }
 
-export default withTM(["@paradox/api", "@paradox/db"])(
-  defineNextConfig({
-    reactStrictMode: true,
-    swcMinify: true,
-  })
+export default withTM(['@paradox/api', '@paradox/db'])(
+	defineNextConfig({
+		reactStrictMode: true,
+		swcMinify: true,
+		async headers() {
+			return [
+				{
+					// matching all API routes
+					source: '/api/:path*',
+					headers: [
+						{key: 'Access-Control-Allow-Credentials', value: 'true'},
+						{key: 'Access-Control-Allow-Origin', value: '*'},
+						{
+							key: 'Access-Control-Allow-Methods',
+							value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+						},
+						{
+							key: 'Access-Control-Allow-Headers',
+							value:
+								'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+						},
+					],
+				},
+			];
+		},
+	}),
 );
