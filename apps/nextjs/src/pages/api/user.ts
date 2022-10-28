b5a6446f0a7970c161c543944091c18d0aa9e95c
henribruvier <henri.bruvier@gmail.com>
2022-10-28 16:46:43 +0200

    feat(api): :sparkles: allow cors for api

--- apps/nextjs/src/pages/api/user.ts
+++ apps/nextjs/src/pages/api/user.ts
@@ -1,12 +1,36 @@
 // src/pages/api/examples.ts
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {prisma} from '@paradox/db';
+import Cors from 'cors';
 
 interface UserRequest extends NextApiRequest {
 	body: string;
 }
 
+const cors = Cors({
+	methods: ['POST', 'GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+const runMiddleware = (
+	req: NextApiRequest,
+	res: NextApiResponse,
+	fn: Function,
+) => {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result: any) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+};
+
 const handler = async (req: UserRequest, res: NextApiResponse) => {
+	await runMiddleware(req, res, cors);
 	console.log('new request', JSON.parse(req.body));
 	if (req.method !== 'POST') {
 		return res.status(405).json({message: 'Method not allowed'});
