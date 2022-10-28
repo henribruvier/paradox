3cc302ea7367b7b7827a4843eee5e9b067add440
henribruvier <henri.bruvier@gmail.com>
2022-10-28 12:05:00 +0200

    feat(web): :sparkles: update head

--- apps/nextjs/src/components/layout/web-layout.tsx
+++ apps/nextjs/src/components/layout/web-layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import * as React from 'react';
 import {Navbar} from '../navbar';
 
@@ -7,9 +8,16 @@ type WebLayoutProps = {
 
 export const WebLayout = ({children}: WebLayoutProps) => {
 	return (
-		<div className='bg-gradient-to-b from-white via-white to-indigo-200 min-h-screen min-w-full'>
-			<Navbar />
-			{children}
-		</div>
+		<>
+			<Head>
+				<title>Paradox</title>
+				<meta name='description' content='Escape Game' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<div className='bg-gradient-to-b from-white via-white to-indigo-200 min-h-screen min-w-full'>
+				<Navbar />
+				{children}
+			</div>
+		</>
 	);
 };
